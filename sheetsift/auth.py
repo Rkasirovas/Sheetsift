cd31345dc80800266ce54d70b935ab6383051f41
Rokas Kasirovas <rokas63@yahoo.com>
2025-04-10 17:02:58 +0300

    Pakoreguota vartotojo registracija ir jungimasis

--- sheetsift/auth.py
+++ sheetsift/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask import Blueprint, render_template, redirect, url_for, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 from .models import User
@@ -11,18 +11,16 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            return render_template('register.html', message='Toks vartotojas jau yra')
+
         hashed_pw = generate_password_hash(password)
-
-        # Patikrinam ar toks naudotojas jau egzistuoja
-        existing_user = User.query.filter_by(username=username).first()
-        if existing_user:
-            flash('Vartotojo vardas jau egzistuoja.', 'danger')
-            return redirect(url_for('auth.register'))
-
         new_user = User(username=username, password=hashed_pw)
         db.session.add(new_user)
         db.session.commit()
-        flash('Registracija sėkminga. Galite prisijungti.', 'success')
+
         return redirect(url_for('auth.login'))
 
     return render_template('register.html')
@@ -36,10 +34,9 @@ def login():
         user = User.query.filter_by(username=username).first()
         if user and check_password_hash(user.password, password):
             login_user(user)
-            flash('Sėkmingai prisijungėte!', 'success')
             return redirect(url_for('main.index'))
         else:
-            flash('Neteisingas prisijungimo vardas arba slaptažodis.', 'danger')
+            return render_template('login.html', message='Neteisingas vardas ar slaptažodis')
 
     return render_template('login.html')
 
@@ -47,5 +44,4 @@ def login():
 @login_required
 def logout():
     logout_user()
-    flash('Atsijungėte.', 'info')
     return redirect(url_for('auth.login'))
