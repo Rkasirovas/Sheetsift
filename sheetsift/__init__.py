e0e5f78fc729d3527742661a7f2292e06c2e74d3
Rokas Kasirovas <rokas63@yahoo.com>
2025-04-10 22:02:14 +0300

    sukurtas flask_admin

--- sheetsift/__init__.py
+++ sheetsift/__init__.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user
+from flask_bcrypt import Bcrypt
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from wtforms import PasswordField
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+bcrypt = Bcrypt()
+admin = Admin(name='Admin', template_mode='bootstrap4')
 
 login_manager.login_view = 'auth.login'
 login_manager.login_message_category = 'info'
@@ -17,6 +23,31 @@ def create_app(config=None):
 
     db.init_app(app)
     login_manager.init_app(app)
+    bcrypt.init_app(app)
+    admin.init_app(app)
+
+    from .models import User
+
+    class AdminAccessView(ModelView):
+        def is_accessible(self):
+            return current_user.is_authenticated and current_user.username == 'admin'
+
+        def inaccessible_callback(self, name, **kwargs):
+            return redirect(url_for('auth.login', next=request.url))
+
+    class SecureUserAdmin(AdminAccessView):
+        form_excluded_columns = ['password']
+
+        def scaffold_form(self):
+            form_class = super().scaffold_form()
+            form_class.new_password = PasswordField('Naujas slaptažodis')
+            return form_class
+
+        def on_model_change(self, form, model, is_created):
+            if form.new_password.data:
+                model.password = bcrypt.generate_password_hash(form.new_password.data).decode('utf-8')
+
+    admin.add_view(SecureUserAdmin(User, db.session))
 
     from .routes import main
     from .auth import auth
@@ -25,4 +56,3 @@ def create_app(config=None):
     app.register_blueprint(auth)
 
     return app
-
--- sheetsift/auth.py
+++ sheetsift/auth.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 from .models import User
-from . import db
+from . import db, bcrypt
 
 auth = Blueprint('auth', __name__)
 
@@ -14,31 +13,32 @@ def register():
         user = User.query.filter_by(username=username).first()
 
         if user:
-            return render_template('register.html', message='Toks vartotojas jau yra')
+            return render_template('register.html', message='Toks vardas jau naudojamas')
 
-        hashed_pw = generate_password_hash(password)
+        hashed_pw = bcrypt.generate_password_hash(password).decode('utf-8')
         new_user = User(username=username, password=hashed_pw)
         db.session.add(new_user)
         db.session.commit()
 
-        return redirect(url_for('auth.login'))
+        return redirect(url_for('auth.login', success_message='Registracija sėkminga! Galite prisijungti'))
 
     return render_template('register.html')
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    success_message = request.args.get('success_message')
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
         user = User.query.filter_by(username=username).first()
-        if user and check_password_hash(user.password, password):
+        if user and bcrypt.check_password_hash(user.password, password):
             login_user(user)
             return redirect(url_for('main.index'))
         else:
             return render_template('login.html', message='Neteisingas vardas ar slaptažodis')
 
-    return render_template('login.html')
+    return render_template('login.html', success_message=success_message)
 
 @auth.route('/logout')
 @login_required
