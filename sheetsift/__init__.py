9b64c75e5dc7f0c1d0ce27643f3d1fadc45fa3ff
Rokas Kasirovas <rokas63@yahoo.com>
2025-05-06 20:27:35 +0300

    sutvarkyta init ir models

--- sheetsift/__init__.py
+++ sheetsift/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask, redirect, url_for, request, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, current_user
+from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
-from wtforms import PasswordField
+
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -33,31 +32,11 @@ def create_app(config=None):
     bcrypt.init_app(app)
     admin.init_app(app)
 
-    from .models import User
-
-    class AdminAccessView(ModelView):
-        def is_accessible(self):
-            return current_user.is_authenticated and current_user.username == 'Monty_Wizard_Python'
-
-        def inaccessible_callback(self, name, **kwargs):
-            return redirect(url_for('auth.login', next=request.url))
-
-    class SecureUserAdmin(AdminAccessView):
-        form_excluded_columns = ['password']
-
-        def scaffold_form(self):
-            form_class = super().scaffold_form()
-            form_class.new_password = PasswordField('Naujas slaptažodis')
-            return form_class
-
-        def on_model_change(self, form, model, is_created):
-            if form.new_password.data:
-                model.password = bcrypt.generate_password_hash(form.new_password.data).decode('utf-8')
-
-    admin.add_view(SecureUserAdmin(User, db.session))
-
+    from .models import User, SecureUserAdmin
     from .routes import main
     from .auth import auth
+
+    admin.add_view(SecureUserAdmin(User, db.session))
 
     app.register_blueprint(main)
     app.register_blueprint(auth)
--- sheetsift/models.py
+++ sheetsift/models.py
@@ -1,5 +1,8 @@
-from . import db, login_manager
-from flask_login import UserMixin
+from . import db, login_manager, bcrypt
+from flask_login import UserMixin, current_user
+from flask_admin.contrib.sqla import ModelView
+from wtforms import PasswordField
+from flask import redirect, request, url_for
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -14,4 +17,26 @@ class User(UserMixin, db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+class AdminAccessView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.username == 'Monty_Wizard_Python'
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('auth.login', next=request.url))
+
+
+class SecureUserAdmin(AdminAccessView):
+    form_excluded_columns = ['password']
+
+    def scaffold_form(self):
+        form_class = super().scaffold_form()
+        form_class.new_password = PasswordField('Naujas slaptažodis')
+        return form_class
+
+    def on_model_change(self, form, model, is_created):
+        if form.new_password.data:
+            model.password = bcrypt.generate_password_hash(form.new_password.data).decode('utf-8')
+
 
